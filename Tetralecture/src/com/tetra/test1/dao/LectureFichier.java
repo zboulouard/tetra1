858b39ea26da167c1082a54b1aa3e16c89421926
Zakaria Boulouard <zboulouard@gmail.com>
2015-03-17 18:29:05 +0100

    Affichage de la matrice sans les noms

--- Tetralecture/src/com/tetra/test1/dao/LectureFichier.java
+++ Tetralecture/src/com/tetra/test1/dao/LectureFichier.java
@@ -3,33 +3,45 @@ package com.tetra.test1.dao;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LectureFichier {
 	
 	public void lireFichier() {
 		
-double startTime = System.currentTimeMillis();
+		double startTime = System.currentTimeMillis();
 		
 		BufferedReader br = null;
+		
+		List<String> listeAuteurs = new ArrayList<String>();
 		 
 		try {
  
 			String sCurrentLine;
  
 			br = new BufferedReader(new FileReader("src/com/tetra/test1/source/A-A.1"));
- 
-//			while ((sCurrentLine = br.readLine()) != null) {
-//				System.out.println(sCurrentLine);
-//			}
 			
 			sCurrentLine = br.readLine();
 			
-			System.out.println(sCurrentLine);
+			System.out.println("*******************Lignes*******************");
 			
-			String[] split = sCurrentLine.split(" ");
+			String[] nomsAuteurs = sCurrentLine.split(" ");
 			
-			for(String ss : split) {
-				System.out.println(ss);
+			for(String auteur : nomsAuteurs) {
+				System.out.println(auteur);
+				listeAuteurs.add(auteur);
+			}
+			
+			System.out.println("*******************Colonnes*******************");
+			
+			while ((sCurrentLine = br.readLine()) != null) {
+				String[] colonnes = sCurrentLine.split(" ");
+				for(int i=1; i<colonnes.length; i++) {
+					String contenu = colonnes[i] + " ";
+					System.out.print(contenu);
+				}
+				System.out.println();
 			}
  
 		} catch (IOException e) {
